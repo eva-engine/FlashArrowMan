e778ea28256110a35b6ab42105803a27195715b3
mingfei.fmf <mingfei.fmf@alibaba-inc.com>
2021-10-18 02:46:13 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import "@babel/polyfill";
+
 import React from 'react';
 import { loadPage } from "./page";
 
-window.React = React
-// import VConsole from 'vconsole';
 
-// new VConsole()
+window.React = React
+import VConsole from 'vconsole';
+
+new VConsole()
 
 loadPage();
