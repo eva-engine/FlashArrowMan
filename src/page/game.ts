9dfbd8919a38b23b79829874771d3b092034de4b
mingfei.fmf <mingfei.fmf@alibaba-inc.com>
2021-10-20 21:42:48 +0800

    邀请提示

--- src/page/game.ts
+++ src/page/game.ts
@@ -233,9 +233,44 @@ export class SingleGame {
   }
 
   enemyHp = 80
+  hasEnergy: any = null
   reloadHome(e: HomeMsgStruct) {
     this.player.emit('onAttack');
-    this.enemyName = e.data.users.find(({ name }) => name !== netPlayer.name)?.name || this.enemyName
+    const energy = e.data.users.find(({ name }) => name !== netPlayer.name)
+    this.hasEnergy = !!energy
+    this.enemyName = energy?.name || this.enemyName
+    if (this.hasEnergy) {
+      this.hideInviteTip()
+    }
+  }
+
+  inviteTipGO:GameObject
+  
+  showInviteTip () {
+    if (this.hasEnergy) {
+      return
+    }
+    this.inviteTipGO = new GameObject('inviteTip', {
+      size: {
+        width: 669,
+        height: 210,
+      },
+      position: {
+        x: 0,
+        y: 0
+      }
+      
+    })
+    this.inviteTipGO.addComponent(new Img({
+      resource: 'invite'
+    }))
+    game.scene.addChild(this.inviteTipGO)
+
+  }
+  
+  hideInviteTip () {
+    this.inviteTipGO?.destroy()
+    this.inviteTipGO = undefined
   }
 
   initNetReactive() {
@@ -424,6 +459,9 @@ export class SingleGame {
     this.initNetReactive();
     this.reloadHome(e);
     this.ready();
+    setTimeout(()=>{
+      this.showInviteTip()
+    }, 10000)
   }
 
   ready() {
